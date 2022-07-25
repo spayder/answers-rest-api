cefe3dce980fbb6e922f99dc45c8c53b80b83bfb
Piotr <gawronski.p@gmail.com>
2022-07-25 22:20:24 +0200

    Finished handler for post answer

--- internal/transport/http/answers.go
+++ internal/transport/http/answers.go
@@ -2,24 +2,34 @@ package http
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/spayder/answers-rest-api/internal/answer"
+	"log"
 	"net/http"
 )
 
 type AnswerService interface {
-	GetAnswers(context.Context) ([]answer.Answer, error)
 	PostAnswer(context.Context, answer.Answer) (answer.Answer, error)
 	GetAnswer(context.Context, string) (answer.Answer, error)
 	UpdateAnswer(context.Context, string, answer.Answer) (answer.Answer, error)
-	DeleteAnswer(context.Context, answer.Answer) error
-}
-
-func (h *Handler) GetAnswers(w http.ResponseWriter, r *http.Request) {
-
+	DeleteAnswer(context.Context, string) error
 }
 
 func (h *Handler) PostAnswer(w http.ResponseWriter, r *http.Request) {
+	var ans answer.Answer
+	if err := json.NewDecoder(r.Body).Decode(&ans); err != nil {
+		return
+	}
 
+	ans, err := h.Service.PostAnswer(r.Context(), ans)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(ans); err != nil {
+		panic(err)
+	}
 }
 
 func (h *Handler) GetAnswer(w http.ResponseWriter, r *http.Request) {
--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -37,7 +37,6 @@ func (h *Handler) mapRoutes() {
 		fmt.Fprintf(writer, "hello")
 	})
 
-	h.Router.HandleFunc("/api/answers", h.GetAnswers).Methods("GET")
 	h.Router.HandleFunc("/api/answers", h.PostAnswer).Methods("POST")
 	h.Router.HandleFunc("/api/answers/{id}", h.GetAnswer).Methods("GET")
 	h.Router.HandleFunc("/api/answers/{id}", h.UpdateAnswer).Methods("PUT")
